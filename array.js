297b8afc26ef68ebc2d1045ea161c8445989bcd1
zero9527 <zero8297183@163.com>
2020-01-05 22:56:20 +0800

    array reduce

--- array.js
+++ array.js
@@ -223,17 +223,17 @@ Array.prototype._reduce = function(cb) {
   var arr = this;
   var i = 0;
   var cb = arguments[0];  // 第一个参数，回调函数
-  var cur = arguments[1] || null; // 第二个参数
+  var acc = arguments[1] || null; // 第二个参数
 
   while(i < arr.length) {
-    cur = cb(cur, arr[i], i, arr);
+    acc = cb(acc, arr[i], i, arr);
     i++;
   }
 
-  return cur;
+  return acc;
 }
 
-// 求和
+// // 求和
 // var list = [1,2,3,4,5,6,7,8,9];
 // var result = list._reduce((acc, cur) => acc + cur, 0);
 // console.log(result); // 45
